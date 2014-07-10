7d4f254436d88a082f0fdd8b6b14243fdf7ee5e1
Ery <erylei@gmail.com>
2014-07-10 13:29:32 +0800

    Smart header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def get_fetch_nav_class
+    if ['show','index','download'].include?(action_name)
+      return 'active'
+    end
+  end
+
+  def get_share_nav_class
+    if ['new','create'].include?(action_name)
+      return 'active'
+    end
+  end
+
 end
--- todo.md
+++ todo.md
@@ -1,6 +1,5 @@
 
 
-+ 左上角log(FetchShare) 是超链接 链接 home
 + 下载页面 添加删除按钮
 + 删除的时候 删除磁盘文件
 + 脚本 rake remove_overdue_archive 删除过去数据
@@ -13,3 +12,6 @@
 + 文件上传 基于 ruby-gem
 + 文件下载 基于 ruby-gem
 
+OK
+-------------------
++ 左上角log(FetchShare) 是超链接 链接 home
