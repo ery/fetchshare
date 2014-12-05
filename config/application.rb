de9db1057d83c8c4287ab5aaeaad40c5bb8248ed
Ery <erylei@gmail.com>
2014-12-05 09:48:12 +0800

    Refactor archive_file

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Fetchshare
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths << "#{config.root}/lib/fetchshare"
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Beijing'
---
+++ lib/fetchshare/archive_file.rb
@@ -0,0 +1,33 @@
+module ArchiveFile
+
+  def self.save_file(archive, upload_file)
+    return unless upload_file
+
+    create_file_folder
+
+    random_number = SecureRandom.random_number(10000)
+    file_name     = "#{random_number}_#{upload_file.original_filename}"
+    file_path     = get_file_path(file_name)
+
+    File.open(file_path, 'wb') do |file|
+      file.write(upload_file.read)
+    end
+
+    archive.file_name        = file_name
+    archive.file_origin_name = upload_file.original_filename
+  end
+
+  def self.get_file_path(file_name)
+    file_path = Rails.root.join('public', 'storage', file_name)
+    return file_path
+  end
+
+  def self.create_file_folder
+    folder_path = Rails.root.join('public', 'storage')
+
+    return if Dir.exists?(folder_path)
+
+    FileUtils.mkdir_p(folder_path)
+  end
+
+end
